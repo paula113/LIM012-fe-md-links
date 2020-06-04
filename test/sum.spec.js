d9aa8ddcb336931e21415f2cfcd916f4cecab767
paula113 <paulapierina96@gmail.com>
2020-06-04 17:20:00 -0500

    absolute path
    function toAbsolute has passed its test succesfullyreturning an array of pure absolutes paths. writing this test I learn that toBe checks for the exact aquality whether toEqual will recursively check every field in an object or array

--- test/sum.spec.js
+++ test/sum.spec.js
@@ -4,13 +4,19 @@ import { toAbsolute } from '../lib/sum';
 const paths = {
   mixedPaths: ['/Users/Drive/Documents/LABORAToRIa/LIM012-fe-md-links/links', 'links/testLinks01.md', 'links/testLinks02.md', 'links/morefiles/app.js', 'links/morefiles/morefiles01.md'],
 
-  absPaths: [],
+  absPaths: [
+    '/Users/Drive/Documents/LABORAToRIa/LIM012-fe-md-links/links',
+    '/Users/Drive/Documents/LABORAToRIa/LIM012-fe-md-links/links/testLinks01.md',
+    '/Users/Drive/Documents/LABORAToRIa/LIM012-fe-md-links/links/testLinks02.md',
+    '/Users/Drive/Documents/LABORAToRIa/LIM012-fe-md-links/links/morefiles/app.js',
+    '/Users/Drive/Documents/LABORAToRIa/LIM012-fe-md-links/links/morefiles/morefiles01.md',
+  ],
 };
 const { mixedPaths, absPaths } = paths;
 
 describe('Checks for absolute paths if not converts them', () => {
   test('It should return absolute paths', () => {
-    expect(toAbsolute(mixedPaths)).toBe(absPaths);
+    expect(toAbsolute(mixedPaths)).toEqual(absPaths);
   });
   // test('object assignment', () => {
   //   const data = { one: 1 };
